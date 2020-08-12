cb38cd08adf9aef92885f3a1798000491f0d184a
dev <crawl3r@protonmail.com>
2020-08-12 13:21:55 -0700

    Added more comments for my future ref

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # learning_rust
 
 A repository of rust projects and small tools that I have written for whatever reason. They likely have a point and cover a new topic, but if they aren't self explanatory I'll include a description below or in the dir.
+
+### reader
+A quick script that basically just reads a file line by line. I assume it doesn't load everything into memory at once, works like a stream instead. Single threaded but loops through rockyou nice and quickly (so far)!
--- reader/src/main.rs
+++ reader/src/main.rs
@@ -6,7 +6,7 @@ use std::io::BufReader;
 use std::io::BufRead;
 
 fn main() {
-    // Parse the cli args
+    // Parse the cli args, error out and show usage if not ran correctly
     let arguments: Vec<String> = env::args().collect();
     if arguments.len() != 2 {
         println!("Usage: {} file.txt", arguments[0]);
@@ -14,25 +14,32 @@ fn main() {
     }
 
     // Access the target file
-    let file_name: String = arguments[1].clone();
+    let file_name: String = arguments[1].clone(); // grab the string from the arguments
     let res_file: Result<File, io::Error> = File::open(file_name);
-    let f: File = match res_file {
-        Ok(file) => file,
+    let f: File = match res_file { // match is basically the 'switch case' of rust.
+		// the res_fule is of type Result, a default enum in Rust with Ok and Err
+        Ok(file) => file, // return the file to 'f'
         Err(err) => {
             println!("Err: opening a file: {}", err);
             process::exit(99);
         },
     };
 
-    // read file line by line, hopefully it doesn't pull everything in memory at once
+	// declare the count to be mutable so we can read/write to the value, right?
+	// mutable seems like an inverted const.
 	let mut count = 0;
+
+    // read file line by line, hopefully it doesn't pull everything in memory at once
     let reader: BufReader<File> = BufReader::new(f);
+
+	// loops the file, line by line through the buffered reader. Ideally, reader isn't the whole file in memory!
     for res_line in reader.lines() {
 		// Might slow down the program a little without the output BUT shows progression
 		if count % 1000000 == 0{
 			println!("Hit milestone: {}", count);		
 		}
 		
+		// "switch case" again on the Result enum, two otions Ok and Err only
         match res_line {
             Ok(_) => {
 				// increment our count as we hit each line
